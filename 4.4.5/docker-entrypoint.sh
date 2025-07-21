92fd689aee1033b0b19365d1e6222bc713ad3cb6
Jure Mekiš <jure.mekis@igea.si>
2025-07-21 08:52:02 +0200

    4.4.5/docker-entrypoint.sh ureditev data dira pred zagonom

--- 4.4.5/docker-entrypoint.sh
+++ 4.4.5/docker-entrypoint.sh
@@ -5,6 +5,27 @@ export JAVA_OPTIONS="${JAVA_OPTS} ${GN_CONFIG_PROPERTIES}"
 
 GN_BASE_DIR=/opt/geonetwork
 
+### ureditev data dira
+echo "Deleting '$DATA_DIR/wro4j-cache.*' files..."
+rm -f $DATA_DIR/wro4j-cache.*
+echo "Deleting '$DATA_DIR/data/resources/htmlcache/formatter-cache/info-store.*' files..."
+rm -f $DATA_DIR/data/resources/htmlcache/formatter-cache/info-store.*
+echo "Deleting '$DATA_DIR/harvester*.log' files..."
+rm -f $DATA_DIR/harvester*.log
+echo "Removing file '$DATA_DIR/index/taxonomy/write.lock', if exists..."
+rm -f $DATA_DIR/index/taxonomy/write.lock
+echo "Removing files '$DATA_DIR/index/index/*/write.lock', if they exist..."
+rm -f $DATA_DIR/index/index/*/write.lock
+
+echo "Replacing data dir formatters, resources, config ..."
+rm -rf $DATA_DIR/data/formatter/*
+find $DATA_DIR/data/resources/ -mindepth 1 -maxdepth 1 -type d -not -name images -exec rm -rf {} \;
+rm -rf $DATA_DIR/config/*
+cp -pr $GN_BASE_DIR/WEB-INF/data/data/formatter $DATA_DIR/data
+cp -pr $GN_BASE_DIR/WEB-INF/data/data/resources $DATA_DIR/data
+cp -pr $GN_BASE_DIR/WEB-INF/data/config $DATA_DIR
+###
+
 if ! command -v -- "$1" >/dev/null 2>&1 ; then
 	set -- java -jar "$JETTY_HOME/start.jar" "$@"
 fi
